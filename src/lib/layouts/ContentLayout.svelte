ceab416a55f89c217621d480d4c34adc2fe700ad
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-02-26 18:11:13 +0700

    feat: add Component property to ContentLayout

--- src/lib/layouts/ContentLayout.svelte
+++ src/lib/layouts/ContentLayout.svelte
@@ -1,3 +1,12 @@
-<div class="min-h-[92vh] flex flex-col gap-4 items-center justify-center">
+<script>
+	export let isAligned = true;
+	export let isJustified = true;
+</script>
+
+<div
+	class="min-h-[92vh] flex flex-col gap-4s"
+	class:items-center={isAligned}
+	class:justify-center={isJustified}
+>
 	<slot />
 </div>
